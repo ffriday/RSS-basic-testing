5f5abb37cb38dbc84d8e672eb25a68610feb2159
mitrich <4irtim@gmail.com>
2024-02-08 23:23:25 +0700

    feat: 08-snapshot-testing task solved

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,34 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+type LinkedListNode<T> = {
+  value: T | null;
+  next: LinkedListNode<T> | null;
+};
+
+const makeList = <T>(elements: T[]): LinkedListNode<T> => {
+  if (!elements.length) {
+    return { value: null, next: null };
+  }
+
+  const [head, ...rest] = elements;
+
+  return {
+    value: head ?? null,
+    next: generateLinkedList(rest),
+  };
+};
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const arr = [1, 2, 3, 4, 5, 6, 7];
+    expect(generateLinkedList(arr)).toStrictEqual(makeList(arr));
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const arr = ['foo', 'bar', 'baz'];
+    expect(generateLinkedList(arr)).toMatchSnapshot();
   });
 });
